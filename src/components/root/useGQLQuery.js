5b01f9534afaed3f398714cb4936ae25f23f0298
mugishaisaac0508@gmail.com <mugishaisaac0508@gmail.com>
2023-02-08 23:22:08 +0200

    [feat: useGQLQuery] fetching data from endpoint into a react useHook method

--- src/components/root/useGQLQuery.js
+++ src/components/root/useGQLQuery.js
@@ -1,2 +1,9 @@
 import {useQuery} from 'react-query';
 import {GraphQLClient,request} from 'graphql-request';
+
+
+export const useGQLQuery = (key,query,variables,config={})=>{
+   const endpoint = 'https://countries.trevorblades.com/';
+   const fetchData = async()=> await request(endpoint,query,variables);
+   return useQuery(key,fetchData,config);
+}
