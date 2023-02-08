c0d679090aa3d3df948a89088362cabbd2447597
mugishaisaac0508@gmail.com <mugishaisaac0508@gmail.com>
2023-02-08 23:52:42 +0200

    [feat: useGQLQuery.ts] review on endpoints that needs token

--- src/components/root/useGQLQuery.ts
+++ src/components/root/useGQLQuery.ts
@@ -4,6 +4,17 @@ import {GraphQLClient,request} from 'graphql-request';
 
 export const useGQLQuery = (key:string,query:any,variables:any,config={})=>{
    const endpoint = 'https://countries.trevorblades.com/';
+
+   // for auth endpoints
+
+   // const headers = {
+   //    headers:{
+   //       authorization: `Bearer token goes here`
+   //    }
+   // }
+   // const graphQlClient = new GraphQLClient(endpoint,headers);
+   
+
    const fetchData = async()=> await request(endpoint,query,variables);
    return useQuery(key,fetchData,config);
 }
